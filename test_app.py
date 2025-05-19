fdf28b5512ab70af6974e69a5d2f56fbd062df1d
avan <mr.mkbox@gmail.com>
2025-05-19 23:32:08 +0000

    pytest checked

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ import time
 
 def test_time_route():
     # Запуск приложения в фоне
-    proc = subprocess.Popen(["python", "app.py"])
+    proc = subprocess.Popen(["python3", "app.py"])
     time.sleep(2)  # Даем время запуску сервера
 
     response = requests.get("http://localhost:5000/time")
