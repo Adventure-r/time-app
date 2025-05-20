8794c3d69959fad18302fb2f49055e0b8915f61d
avan <mr.mkbox@gmail.com>
2025-05-20 00:00:03 +0000

    Changed test

--- test_app.py
+++ test_app.py
@@ -1,3 +1,18 @@
+import requests
+import subprocess
+import time
+
+def test_time_route():
+    # Запуск приложения в фоне
+    proc = subprocess.Popen(["python", "app.py"])
+    time.sleep(2)  # Даем время запуску сервера
+
+    response = requests.get("http://localhost:5000/time")
+    data = response.json()
+    assert data["time"] != 0, "Time should not be zero"
+
+    proc.terminate()
+
 def test_metrics_route():
     proc = subprocess.Popen(["python3", "app.py"])
     time.sleep(2)
